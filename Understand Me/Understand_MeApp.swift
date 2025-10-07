3b2cc7605c7d2dc3a38327f0092cd5612a148fde
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-10-07 18:20:19 +0900

    create project

---
+++ Understand Me/Understand_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Understand_MeApp.swift
+//  Understand Me
+//
+//  Created by cmStudent on 2025/10/07.
+//
+
+import SwiftUI
+
+@main
+struct Understand_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
