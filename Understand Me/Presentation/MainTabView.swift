8dd077fd5aae6a7b54b0e04b5694883604291b49
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-10-11 15:34:37 +0900

    MainTabViewをLoginViewを表示するように修正

---
+++ Understand Me/Presentation/MainTabView.swift
@@ -0,0 +1,76 @@
+//
+//  MainTabView.swift
+//  Understand Me
+//
+//  Created by cmStudent on 2025/10/08.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    
+    @State private var selectedTab: Int = 0
+    @State private var authDataResult: AuthDataResultModel? = nil
+    
+    var body: some View {
+        if authDataResult == nil {
+            LoginInView { authDataResult in
+                withAnimation(.spring) {
+                    self.authDataResult = authDataResult
+                }
+            }
+            .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .bottom)))
+
+        } else {
+            tabView
+        }
+    }
+    
+    private var tabView: some View {
+        TabView(selection: $selectedTab) {
+            NavigationStack{
+                HomeView(selectedTab: $selectedTab)
+            }
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("ホーム")
+            }
+            .tag(0)
+            
+            
+            NavigationStack{
+                ClassListView()
+                
+            }
+            .tabItem {
+                Image(systemName: "graduationcap")
+                Text("クラス")
+            }
+            
+            NavigationStack{
+                HomeworkListView()
+                
+            }
+            .tabItem {
+                Image(systemName: "list.bullet.rectangle.portrait")
+                Text("課題")
+            }
+            
+            
+            
+            NavigationStack{
+                ProfileView(authDataResult: $authDataResult)
+            }
+            .tabItem {
+                Image(systemName: "person")
+                Text("プロフィール")
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+
+    }
+}
+
+#Preview {
+    MainTabView()
+}
