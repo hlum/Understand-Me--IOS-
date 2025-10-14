7f6960fe09eb80ea81a3f662545cff27fe9d1a68
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-10-14 17:58:18 +0900

    HomeworkDetailViewModelをファイル分ける

--- Understand Me/Presentation/ViewComponents/HomeworkDetailView.swift
+++ Understand Me/Presentation/ViewComponents/HomeworkDetailView.swift
@@ -6,53 +6,6 @@
 //
 
 import SwiftUI
-import Combine
-
-class HomeworkDetailViewModel: ObservableObject {
-    @Published var homework: HomeworkWithStatus?
-    @Published var classDetail: Class?
-    
-    private let homeworkUseCase: HomeworkUseCase
-    private let classUseCase: ClassUseCase
-    
-    init(homeworkUseCase: HomeworkUseCase, classUseCase: ClassUseCase) {
-        self.homeworkUseCase = homeworkUseCase
-        self.classUseCase = classUseCase
-    }
-    
-    
-    @MainActor
-    func loadInfoOfHomework(homeworkID: String) async {
-        await loadHomework(id: homeworkID)
-        if let homework = self.homework {
-            await loadClassDetail(classID: homework.classID)
-        }
-    }
-    
-    
-    @MainActor
-    private func loadHomework(id: String) async {
-        do {
-            homework = try await homeworkUseCase.fetchHomework(id: id)
-        } catch {
-            print("HomeworkDetailViewModel.loadHomework: 宿題の取得に失敗しました。\(error.localizedDescription)")
-        }
-    }
-    
-    
-    
-    @MainActor
-    private func loadClassDetail(classID: String) async {
-        do {
-            self.classDetail = try await classUseCase.fetchClass(id: classID)
-        } catch {
-            print("HomeworkDetailViewModel.loadClassDetail: クラス情報の取得に失敗しました。\(error.localizedDescription)")
-        }
-    }
-    
-    
-    
-}
 
 struct HomeworkDetailView: View {
     var id: String
