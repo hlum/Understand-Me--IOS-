5a86067fad93214b611513db38670c36460faaf0
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-10-24 12:24:02 +0900

    <feat> ClassHomeworkViewにFilter Buttonを追加

--- Understand Me/Presentation/Views/ClassHomeworkView.swift
+++ Understand Me/Presentation/Views/ClassHomeworkView.swift
@@ -14,23 +14,52 @@ struct ClassHomeworkView: View {
     var classID: String
     
     
-    init(classID: String) {
+    init(classID: String,
+         homeworkRepo: HomeworkRepository = LollipopHomeworkRepository(),
+         authenticationRepo:AuthenticationRepository = FirebaseAuthenticationRepository(),
+         classRepo: ClassRepository = LollipopClassRepository()
+    ) {
         self.classID = classID
         _viewModel = StateObject(wrappedValue: ClassHomeworkViewModel(
-            homeworkUseCase: HomeworkUseCase(homeworkRepository: LollipopHomeworkRepository()),
-            authenticationUseCase: AuthenticationUseCase(authenticationRepository: FirebaseAuthenticationRepository()),
-            classUseCase: ClassUseCase(classRepository: LollipopClassRepository()),
+            homeworkUseCase: HomeworkUseCase(homeworkRepository: homeworkRepo),
+            authenticationUseCase: AuthenticationUseCase(authenticationRepository: authenticationRepo),
+            classUseCase: ClassUseCase(classRepository: classRepo),
             classID: classID
         ))
     }
     
+    @State private var selectedFilter: HomeworkFilterOption = .all
+    
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            ForEach(viewModel.homeworks) { homework in
-                HomeworkListItemView(id: homework.id, title: homework.title, dueDate: homework.dueDate ?? Date(), state: homework.submissionState)
+        
+        VStack {
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 10) {
+                    ForEach(HomeworkFilterOption.allCases, id: \.self) { option in
+                        FilterButton(title: option.displayName, isSelected: selectedFilter == option) {
+                            
+                        }
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 30)
+            .padding(10)
+            .cornerRadius(10)
+            
+            
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    ForEach(viewModel.homeworks) { homework in
+                        HomeworkListItemView(id: homework.id, title: homework.title, dueDate: homework.dueDate ?? Date(), state: homework.submissionState)
+                    }
+                }
+                .padding(.top, 10)
             }
         }
         .navigationTitle(viewModel.classInfo?.name ?? "")
+        .navigationBarTitleDisplayMode(.inline)
         .task {
             await viewModel.loadClassInfos()
             await viewModel.loadHomeworks(classID: classID)
@@ -40,6 +69,11 @@ struct ClassHomeworkView: View {
 
 #Preview {
     NavigationStack {
-        ClassHomeworkView(classID: "IOS プログラミング")
+        ClassHomeworkView(
+            classID: "IOS プログラミング",
+            homeworkRepo: TestHomeworkRepository(),
+            authenticationRepo: TestAuthenticationRepository(),
+            classRepo: TestClassRepository()
+        )
     }
 }
