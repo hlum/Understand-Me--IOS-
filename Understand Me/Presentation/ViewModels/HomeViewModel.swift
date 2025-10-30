6c5eb61f57dd46c18ff57e0a2eb04eaedf1c2b9a
ラワンアウンピョウ <53109660+hlum@users.noreply.github.com>
2025-10-30 13:51:05 +0900

    style: HomeView class list UI (#23)
    * chore : add .DS_Store to gitignore
    
    * chore : ホームViewに dependency injection
    
    Test Repositoryでテストできるようにするため
    
    * refactor : HomeViewModelを別のファイルの分ける
    
    * style: HomeView class list UI
    
    ホームViewのクラスリストをhorizontal scrollにし、画面の上に表示するように変更

---
+++ Understand Me/Presentation/ViewModels/HomeViewModel.swift
@@ -0,0 +1,116 @@
+//
+//  HomeViewModel.swift
+//  Understand Me
+//
+//  Created by cmStudent on 2025/10/30.
+//
+
+
+import Combine
+import OSLog
+
+class HomeViewModel: ObservableObject {
+    private let userDataUseCase: UserDataUseCase
+    private let authenticationUseCase: AuthenticationUseCase
+    private let homeworkUseCase: HomeworkUseCase
+    private let classUseCase: ClassUseCase
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "UnderstandMe", category: "Presentation")
+    
+    @Published var userData: UserData? = nil
+    @Published var homeworks: [HomeworkWithStatus] = []
+    @Published var classes: [Class] = []
+    
+    
+    init(
+        authenticationUseCase: AuthenticationUseCase,
+        userDataUseCase: UserDataUseCase,
+        homeworkUseCase: HomeworkUseCase,
+        classUseCase: ClassUseCase
+    ) {
+        self.userDataUseCase = userDataUseCase
+        self.authenticationUseCase = authenticationUseCase
+        self.homeworkUseCase = homeworkUseCase
+        self.classUseCase = classUseCase
+    }
+    
+    
+    
+    @MainActor
+    func loadUserData() async {
+        guard let authDataResult = await authenticationUseCase.fetchCurrentUser() else {
+            logger.error("AuthDataResultを取得できません。")
+            return
+        }
+        
+        do {
+            self.userData = try await userDataUseCase.fetchUserData(userID: authDataResult.id)
+        } catch {
+            // TODO: UserにAlertで知らせる
+            logger.error("HomeViewModel.loadUserData(): UserDataの取得に失敗しました。")
+        }
+    }
+    
+    
+    
+    @MainActor
+    func loadHomeworks() async {
+        guard let authDataResult = await authenticationUseCase.fetchCurrentUser() else {
+            logger.error("AuthDataResultを取得できません。")
+            return
+        }
+        do {
+            var homeworks = try await homeworkUseCase.fetchHomeworks(studentID: authDataResult.id)
+            
+            // 1. Filter (.completed じゃないものだけ)
+            homeworks = homeworks.filter { $0.submissionState != .completed }
+            
+            
+            // 2. Sort: dueDate(昇順),
+            //    submissionStateの順に並び替え(.notAssigned, .failed, .questionGenerated, .generatingQuestions)
+            homeworks.sort { lhs, rhs in
+                // dueDateのUnwarp
+                let lhsDate = lhs.dueDate ?? .distantFuture
+                let rhsDate = rhs.dueDate ?? .distantFuture
+                
+                
+                if lhsDate != rhsDate {
+                    return lhsDate < rhsDate
+                } else {
+                    
+                    // submissionStateの優先順位を定義
+                    let submissionStatePriority: [HomeworkState: Int] = [
+                        .notAssigned: 0,
+                        .failed: 1,
+                        .questionGenerated: 2,
+                        .generatingQuestions: 3,
+                    ]
+                    
+                    
+                    return (submissionStatePriority[lhs.submissionState] ?? Int.max < submissionStatePriority[rhs.submissionState] ?? Int.max)
+                }
+            }
+            
+            
+            self.homeworks = homeworks
+            
+        } catch {
+            logger.error("HomeViewModel.loadHomeworks(): 宿題の取得に失敗しました。")
+        }
+    }
+    
+    
+    
+    @MainActor
+    func loadClasses() async {
+        guard let authDataResult = await authenticationUseCase.fetchCurrentUser() else {
+            logger.error("AuthDataResultを取得できません。")
+            return
+        }
+        do {
+            self.classes = try await classUseCase.fetchClassList(studentID: authDataResult.id)
+        } catch {
+            logger.error("HomeViewModel.loadClasses(): クラスの取得に失敗しました。")
+        }
+        
+    }
+}
--- Understand Me/Presentation/Views/HomeView.swift
+++ Understand Me/Presentation/Views/HomeView.swift
@@ -6,229 +6,115 @@
 //
 
 import SwiftUI
-import Combine
-import OSLog
-
-class HomeViewModel: ObservableObject {
-    private let userDataUseCase: UserDataUseCase
-    private let authenticationUseCase: AuthenticationUseCase
-    private let homeworkUseCase: HomeworkUseCase
-    private let classUseCase: ClassUseCase
-    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "UnderstandMe", category: "Presentation")
-    
-    @Published var userData: UserData? = nil
-    @Published var homeworks: [HomeworkWithStatus] = []
-    @Published var classes: [Class] = []
-    
-    
-    init(
-        authenticationUseCase: AuthenticationUseCase,
-        userDataUseCase: UserDataUseCase,
-        homeworkUseCase: HomeworkUseCase,
-        classUseCase: ClassUseCase
-    ) {
-        self.userDataUseCase = userDataUseCase
-        self.authenticationUseCase = authenticationUseCase
-        self.homeworkUseCase = homeworkUseCase
-        self.classUseCase = classUseCase
-    }
-    
-    
-    
-    @MainActor
-    func loadUserData() async {
-        guard let authDataResult = await authenticationUseCase.fetchCurrentUser() else {
-            logger.error("AuthDataResultを取得できません。")
-            return
-        }
-        
-        do {
-            self.userData = try await userDataUseCase.fetchUserData(userID: authDataResult.id)
-        } catch {
-            // TODO: UserにAlertで知らせる
-            logger.error("HomeViewModel.loadUserData(): UserDataの取得に失敗しました。")
-        }
-    }
-    
-    
-    
-    @MainActor
-    func loadHomeworks() async {
-        guard let authDataResult = await authenticationUseCase.fetchCurrentUser() else {
-            logger.error("AuthDataResultを取得できません。")
-            return
-        }
-        do {
-            var homeworks = try await homeworkUseCase.fetchHomeworks(studentID: authDataResult.id)
-            
-            // 1. Filter (.completed じゃないものだけ)
-            homeworks = homeworks.filter { $0.submissionState != .completed }
-        
-
-            // 2. Sort: dueDate(昇順),
-            //    submissionStateの順に並び替え(.notAssigned, .failed, .questionGenerated, .generatingQuestions)
-            homeworks.sort { lhs, rhs in
-                // dueDateのUnwarp
-                let lhsDate = lhs.dueDate ?? .distantFuture
-                let rhsDate = rhs.dueDate ?? .distantFuture
-                
-                
-                if lhsDate != rhsDate {
-                    return lhsDate < rhsDate
-                } else {
-                    
-                    // submissionStateの優先順位を定義
-                    let submissionStatePriority: [HomeworkState: Int] = [
-                        .notAssigned: 0,
-                        .failed: 1,
-                        .questionGenerated: 2,
-                        .generatingQuestions: 3,
-                    ]
-                    
-                    
-                    return (submissionStatePriority[lhs.submissionState] ?? Int.max < submissionStatePriority[rhs.submissionState] ?? Int.max)
-                }
-            }
-            
-            
-            self.homeworks = homeworks
-            
-        } catch {
-            logger.error("HomeViewModel.loadHomeworks(): 宿題の取得に失敗しました。")
-        }
-    }
-    
-    
-    
-    @MainActor
-    func loadClasses() async {
-        guard let authDataResult = await authenticationUseCase.fetchCurrentUser() else {
-            logger.error("AuthDataResultを取得できません。")
-            return
-        }
-        do {
-            self.classes = try await classUseCase.fetchClassList(studentID: authDataResult.id)
-        } catch {
-            logger.error("HomeViewModel.loadClasses(): クラスの取得に失敗しました。")
-        }
-        
-    }
-}
 
 struct HomeView: View {
     @Binding var selectedTab: Int
     
-    private let adaptiveColumn = [
-        GridItem(.adaptive(minimum: 160), spacing: 16)
-    ]
-    
-    
     @StateObject private var viewModel: HomeViewModel
     
-    
-    init(selectedTab: Binding<Int>) {
+    // MARK: Init
+    init(
+        selectedTab: Binding<Int>,
+        authenticationRepo: AuthenticationRepository = FirebaseAuthenticationRepository(),
+        userDataRepo: UserDataRepository = LollipopUserDataRepository(),
+        homeworkRepo: HomeworkRepository = LollipopHomeworkRepository(),
+        classRepo: ClassRepository = LollipopClassRepository()
+    ) {
         self._selectedTab = selectedTab
         
         self._viewModel = .init(
             wrappedValue: .init(
-                authenticationUseCase: AuthenticationUseCase(authenticationRepository: FirebaseAuthenticationRepository()),
-                userDataUseCase: UserDataUseCase(
-                    userDataRepository: LollipopUserDataRepository()
-                ),
-                homeworkUseCase: HomeworkUseCase(
-                    homeworkRepository: LollipopHomeworkRepository()
-                ), classUseCase: ClassUseCase(classRepository: LollipopClassRepository())
+                authenticationUseCase: AuthenticationUseCase(authenticationRepository: authenticationRepo),
+                userDataUseCase: UserDataUseCase(userDataRepository: userDataRepo),
+                homeworkUseCase: HomeworkUseCase(homeworkRepository: homeworkRepo),
+                classUseCase: ClassUseCase(classRepository: classRepo)
             )
         )
     }
     
     var body: some View {
         
-        ScrollView(showsIndicators: false) {
-            VStack {
-                
-                header
-                
-                // MARK: - Upcoming Homework
-                VStack(alignment: .leading, spacing: 12) {
-                    Button {
-                        selectedTab = 2
-                    } label: {
-                        HStack {
-                            Text("提出期限が近い課題")
-                                .font(.title2.bold())
-                                .padding(.horizontal)
-                            
-                            Image(systemName: "arrow.forward")
-                                .bold()
-                                .foregroundStyle(.accent.opacity(0.3))
-                            
-                            Spacer()
-                        }
+        VStack {
+            
+            header
+            
+      
+            
+            // MARK: - My Classes
+            VStack(alignment: .leading, spacing: 12) {
+                Button {
+                    selectedTab = 1
+                } label: {
+                    HStack {
+                        Text("マイクラス")
+                            .font(.title2.bold())
+                            .padding(.horizontal)
+                        
+                        Image(systemName: "arrow.forward")
+                            .bold()
+                            .foregroundStyle(.accent.opacity(0.3))
+                        
+                        Spacer()
                     }
-                    
-                    Group {
-                        if !viewModel.homeworks.isEmpty {
-                            
-                            ScrollView(showsIndicators: false ) {
-                                VStack {
-                                    ForEach(viewModel.homeworks) { homework in
-                                        HomeworkListItemView(id: homework.id, title: homework.title, dueDate: homework.dueDate ?? Date(), state: homework.submissionState)
-                                    }
-                                }
-                                .padding(.vertical)
-                            }
-                        } else {
-                            ContentUnavailableView("提出期限が近い課題はありません。", systemImage: "book.closed")
-                                .foregroundStyle(.secondary.opacity(0.7))
-                        }
-                    }
-                    .frame(height: 300)
                 }
+                .foregroundStyle(.primary)
                 
-                
-                // MARK: - My Classes
-                VStack(alignment: .leading, spacing: 12) {
-                    Button {
-                        selectedTab = 1
-                    } label: {
-                        HStack {
-                            Text("マイクラス")
-                                .font(.title2.bold())
-                                .padding(.horizontal)
-                            
-                            Image(systemName: "arrow.forward")
-                                .bold()
-                                .foregroundStyle(.accent.opacity(0.3))
-                            
-                            Spacer()
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack {
+                        ForEach(viewModel.classes) { classItem in
+                            classCell(
+                                classID: classItem.id,
+                                className: classItem.name,
+                                teacherName: classItem.teacherId
+                            )
                         }
                     }
-                    .foregroundStyle(.primary)
-                    
-                    Group {
-                        if !viewModel.classes.isEmpty {
-                            LazyVGrid(columns: adaptiveColumn, spacing: 16) {
-                                ForEach(viewModel.classes) { classItem in
-                                    classCell(
-                                        classID: classItem.id,
-                                        className: classItem.name,
-                                        teacherName: classItem.teacherId
-                                    )
-                                }
-                            }
-                        } else {
-                            ContentUnavailableView("所属するクラスがありません。", systemImage: "book")
-                                .foregroundStyle(.secondary.opacity(0.7))
-                        }
-                    }
-                    .padding(.horizontal)
+                    .padding()
                 }
-                
-                Spacer(minLength: 40)
             }
-            .foregroundStyle(.primary)
+            .frame(maxHeight: 150)
+
+            
+            Spacer()
+            
+            
+            // MARK: - Upcoming Homework
+            VStack(alignment: .leading, spacing: 12) {
+                Button {
+                    selectedTab = 2
+                } label: {
+                    HStack {
+                        Text("提出期限が近い課題")
+                            .font(.title2.bold())
+                            .padding(.horizontal)
+                        
+                        Image(systemName: "arrow.forward")
+                            .bold()
+                            .foregroundStyle(.accent.opacity(0.3))
+                        
+                        Spacer()
+                    }
+                }
+                
+                Group {
+                    if !viewModel.homeworks.isEmpty {
+                        
+                        ScrollView(showsIndicators: false ) {
+                            VStack {
+                                ForEach(viewModel.homeworks) { homework in
+                                    HomeworkListItemView(id: homework.id, title: homework.title, dueDate: homework.dueDate ?? Date(), state: homework.submissionState)
+                                }
+                            }
+                            .padding(.vertical)
+                        }
+                    } else {
+                        ContentUnavailableView("提出期限が近い課題はありません。", systemImage: "book.closed")
+                            .foregroundStyle(.secondary.opacity(0.7))
+                    }
+                }
+            }
         }
+        .foregroundStyle(.primary)
         .task {
             await viewModel.loadUserData()
             await viewModel.loadHomeworks()
@@ -262,9 +148,8 @@ struct HomeView: View {
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.primary.opacity(0.3), lineWidth: 1))
         }
-        .padding()
-        .padding(.top, 20)
-        .padding(.horizontal, 10)
+        .padding(.horizontal,10)
+        .padding(.bottom, 20)
         .background(
             LinearGradient(colors: [.purple, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .opacity(0.3)
@@ -286,10 +171,10 @@ struct HomeView: View {
             }
             .lineLimit(1)
             .padding()
-            .frame(maxWidth: .infinity, minHeight: 60, alignment: .leading)
+            .frame(maxWidth: 200, minHeight: 100, alignment: .leading)
             .background(.background)
             .cornerRadius(20)
-            .shadow(color: .primary.opacity(0.2), radius: 8)
+            .shadow(color: .primary.opacity(0.2), radius: 2)
         })
     }
 }
@@ -297,6 +182,12 @@ struct HomeView: View {
 
 #Preview {
     NavigationStack {
-        HomeView(selectedTab: .constant(1))
+        HomeView(
+            selectedTab: .constant(1),
+            authenticationRepo: TestAuthenticationRepository(),
+            userDataRepo: TestUserRepository(),
+            homeworkRepo: TestHomeworkRepository(),
+            classRepo: TestClassRepository()
+        )
     }
 }
