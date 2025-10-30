bb7df39647dc4ac9c243f7872d86887d8913c145
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-10-30 13:48:29 +0900

    style: HomeView class list UI
    ホームViewのクラスリストをhorizontal scrollにし、画面の上に表示するように変更

--- Understand Me/Presentation/Views/HomeView.swift
+++ Understand Me/Presentation/Views/HomeView.swift
@@ -10,14 +10,9 @@ import SwiftUI
 struct HomeView: View {
     @Binding var selectedTab: Int
     
-    private let adaptiveColumn = [
-        GridItem(.adaptive(minimum: 160), spacing: 16)
-    ]
-    
-    
     @StateObject private var viewModel: HomeViewModel
     
-    
+    // MARK: Init
     init(
         selectedTab: Binding<Int>,
         authenticationRepo: AuthenticationRepository = FirebaseAuthenticationRepository(),
@@ -39,91 +34,87 @@ struct HomeView: View {
     
     var body: some View {
         
-        ScrollView(showsIndicators: false) {
-            VStack {
-                
-                header
-                
-                // MARK: - Upcoming Homework
-                VStack(alignment: .leading, spacing: 12) {
-                    Button {
-                        selectedTab = 2
-                    } label: {
-                        HStack {
-                            Text("提出期限が近い課題")
-                                .font(.title2.bold())
-                                .padding(.horizontal)
-                            
-                            Image(systemName: "arrow.forward")
-                                .bold()
-                                .foregroundStyle(.accent.opacity(0.3))
-                            
-                            Spacer()
-                        }
+        VStack {
+            
+            header
+            
+      
+            
+            // MARK: - My Classes
+            VStack(alignment: .leading, spacing: 12) {
+                Button {
+                    selectedTab = 1
+                } label: {
+                    HStack {
+                        Text("マイクラス")
+                            .font(.title2.bold())
+                            .padding(.horizontal)
+                        
+                        Image(systemName: "arrow.forward")
+                            .bold()
+                            .foregroundStyle(.accent.opacity(0.3))
+                        
+                        Spacer()
                     }
-                    
-                    Group {
-                        if !viewModel.homeworks.isEmpty {
-                            
-                            ScrollView(showsIndicators: false ) {
-                                VStack {
-                                    ForEach(viewModel.homeworks) { homework in
-                                        HomeworkListItemView(id: homework.id, title: homework.title, dueDate: homework.dueDate ?? Date(), state: homework.submissionState)
-                                    }
-                                }
-                                .padding(.vertical)
-                            }
-                        } else {
-                            ContentUnavailableView("提出期限が近い課題はありません。", systemImage: "book.closed")
-                                .foregroundStyle(.secondary.opacity(0.7))
-                        }
-                    }
-                    .frame(height: 300)
                 }
+                .foregroundStyle(.primary)
                 
-                
-                // MARK: - My Classes
-                VStack(alignment: .leading, spacing: 12) {
-                    Button {
-                        selectedTab = 1
-                    } label: {
-                        HStack {
-                            Text("マイクラス")
-                                .font(.title2.bold())
-                                .padding(.horizontal)
-                            
-                            Image(systemName: "arrow.forward")
-                                .bold()
-                                .foregroundStyle(.accent.opacity(0.3))
-                            
-                            Spacer()
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack {
+                        ForEach(viewModel.classes) { classItem in
+                            classCell(
+                                classID: classItem.id,
+                                className: classItem.name,
+                                teacherName: classItem.teacherId
+                            )
                         }
                     }
-                    .foregroundStyle(.primary)
-                    
-                    Group {
-                        if !viewModel.classes.isEmpty {
-                            LazyVGrid(columns: adaptiveColumn, spacing: 16) {
-                                ForEach(viewModel.classes) { classItem in
-                                    classCell(
-                                        classID: classItem.id,
-                                        className: classItem.name,
-                                        teacherName: classItem.teacherId
-                                    )
-                                }
-                            }
-                        } else {
-                            ContentUnavailableView("所属するクラスがありません。", systemImage: "book")
-                                .foregroundStyle(.secondary.opacity(0.7))
-                        }
-                    }
-                    .padding(.horizontal)
+                    .padding()
                 }
-                
-                Spacer(minLength: 40)
             }
-            .foregroundStyle(.primary)
+            .frame(maxHeight: 150)
+
+            
+            Spacer()
+            
+            
+            // MARK: - Upcoming Homework
+            VStack(alignment: .leading, spacing: 12) {
+                Button {
+                    selectedTab = 2
+                } label: {
+                    HStack {
+                        Text("提出期限が近い課題")
+                            .font(.title2.bold())
+                            .padding(.horizontal)
+                        
+                        Image(systemName: "arrow.forward")
+                            .bold()
+                            .foregroundStyle(.accent.opacity(0.3))
+                        
+                        Spacer()
+                    }
+                }
+                
+                Group {
+                    if !viewModel.homeworks.isEmpty {
+                        
+                        ScrollView(showsIndicators: false ) {
+                            VStack {
+                                ForEach(viewModel.homeworks) { homework in
+                                    HomeworkListItemView(id: homework.id, title: homework.title, dueDate: homework.dueDate ?? Date(), state: homework.submissionState)
+                                }
+                            }
+                            .padding(.vertical)
+                        }
+                    } else {
+                        ContentUnavailableView("提出期限が近い課題はありません。", systemImage: "book.closed")
+                            .foregroundStyle(.secondary.opacity(0.7))
+                    }
+                }
+            }
         }
+        .foregroundStyle(.primary)
         .task {
             await viewModel.loadUserData()
             await viewModel.loadHomeworks()
@@ -157,9 +148,8 @@ struct HomeView: View {
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.primary.opacity(0.3), lineWidth: 1))
         }
-        .padding()
-        .padding(.top, 20)
-        .padding(.horizontal, 10)
+        .padding(.horizontal,10)
+        .padding(.bottom, 20)
         .background(
             LinearGradient(colors: [.purple, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .opacity(0.3)
@@ -181,10 +171,10 @@ struct HomeView: View {
             }
             .lineLimit(1)
             .padding()
-            .frame(maxWidth: .infinity, minHeight: 60, alignment: .leading)
+            .frame(maxWidth: 200, minHeight: 100, alignment: .leading)
             .background(.background)
             .cornerRadius(20)
-            .shadow(color: .primary.opacity(0.2), radius: 8)
+            .shadow(color: .primary.opacity(0.2), radius: 2)
         })
     }
 }
